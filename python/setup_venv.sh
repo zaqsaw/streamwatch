70a86930c5885e4208e105ee68020ea729bf1ccd
zaqsaw <zaqsaw@gmail.com>
2022-11-13 04:01:33 -0800

    remove unused ffmpeg download

--- python/setup_venv.sh
+++ python/setup_venv.sh
@@ -5,5 +5,4 @@ python3 -m venv $HOME/venv/streamdl_env
 source $HOME/venv/streamdl_env/bin/activate
 python3 -m pip install wheel
 python3 -m pip install streamlink
-python3 -m pip install ffmpeg-python
 python3 -m pip install mypy
