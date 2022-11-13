87b5302dcf8f7b030ec5e86e391a50187c926a71
zaqsaw <zaqsaw@gmail.com>
2022-11-13 03:41:56 -0800

    add config comments

--- python/config.py
+++ python/config.py
@@ -1,4 +1,12 @@
+# The name of the channel to query
 streamer = ""
+
+# The Oath Authorization can be gotten from a a browser developer console using
+# document.cookie.split("; ").find(item=>item.startsWith("auth-token="))?.split("=")[1]
 auth = ""
+
+# The fully qualified output file to save to
 output = ""
+
+# The amount of time to wait in between queries, in seconds
 period = 60
